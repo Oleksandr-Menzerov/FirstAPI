354b497556b55eb2f96968d10018427e15f73924
Oleksandr Menzerov <Oleksandr_Menzerov@epam.com>
2022-07-13 22:19:22 +0300

    Add project files.

---
+++ Models/FirstAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FirstAPI.Models
+{
+    public class FirstAPIContext : DbContext
+    {
+        public FirstAPIContext(DbContextOptions<FirstAPIContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Rocker> TodoItems { get; set; } = null!;
+    }
+}
---
+++ Models/Rocker.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstAPI.Models
+{
+    public class Rocker
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public string? Band { get; set; }
+    }
+}
